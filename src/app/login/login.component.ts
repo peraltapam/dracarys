e7cfd41281194db97e9f943bb27143f9475a4584
Pamela M. Peralta <pamperalta@gmail.com>
2019-06-19 17:45:23 -0300

    add login template and logic

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.sass']
 })
 export class LoginComponent implements OnInit {
+  user = {username: 'dany', password: 'drogon'};
+  showAlert = false;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  loginHandler(form: NgForm) {
+    console.log('login clicked', form);
+    if(form.value.username === this.user.username &&
+       form.value.password === this.user.password ) {
+      console.log('login successful');
+      this.showAlert = false;
+    } else {
+      this.showAlert = true;
+    }
   }
 
 }
