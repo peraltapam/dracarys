8a91a50a8f89649829a244a9d7953314756f091c
Pamela M. Peralta <pamperalta@gmail.com>
2019-07-08 09:04:37 -0300

    clear login subscription on header

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,19 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 import { AuthService } from 'src/app/login/auth/auth.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.sass']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, OnDestroy {
   isUserLoggedIn: boolean;
+
+  private loginSubscription: Subscription;
 
   constructor(private authService: AuthService ) { }
 
   ngOnInit() {
-    this.authService.loggedIn.subscribe(
+    this.loginSubscription = this.authService.loggedIn.subscribe(
       authenticated => {
         this.isUserLoggedIn = authenticated;
       }
@@ -22,6 +25,10 @@ export class HeaderComponent implements OnInit {
 
   logoutHandler() {
     this.authService.logout();
+  }
+
+  ngOnDestroy() {
+    this.loginSubscription.unsubscribe();
   }
 
 }
